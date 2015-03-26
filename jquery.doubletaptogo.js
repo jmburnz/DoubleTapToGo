1191934ddf7391fa37589d1a82adb3962b781987
Zeno Popovici <zeno@graffino.com>
2015-03-26 19:07:10 +0200

    Add unbind & expand example
    Signed-off-by: Zeno Popovici <zeno@graffino.com>

--- jquery.doubletaptogo.js
+++ jquery.doubletaptogo.js
@@ -1,37 +1,51 @@
-/*
-	Originally by Osvaldas Valutis, www.osvaldas.info	
-	Available for use under the MIT License
+/* 
+Double Tap to Go
+Author: Graffino (http://www.graffino.com)
+Version: 0.3
+Originally by Osvaldas Valutis, www.osvaldas.info	
+Available for use under the MIT License
 */
 
 ;(function($, window, document, undefined) {
-	$.fn.doubleTapToGo = function(params) {
+	$.fn.doubleTapToGo = function(action) {
+
 		if (!('ontouchstart' in window) &&
 			!navigator.msMaxTouchPoints &&
 			!navigator.userAgent.toLowerCase().match( /windows phone os 7/i )) return false;
 
-		this.each(function() {
-			var curItem = false;
-
-			$(this).on('click', function(e) {
-				var item = $(this);
-				if (item[0] != curItem[0]) {
-					e.preventDefault();
-					curItem = item;
-				}
+		if (action === 'unbind') {
+			this.each(function() {
+				$(this).off();
+				$(document).off('click touchstart MSPointerDown', handleTouch);	
 			});
 
-			$(document).on('click touchstart MSPointerDown', function(e) {
-				var resetItem = true,
-					parents = $(e.target).parents();
-
-				for (var i = 0; i < parents.length; i++)
-					if (parents[i] == curItem[0])
-						resetItem = false;
-
-				if(resetItem)
-					curItem = false;
+		} else {
+			this.each(function() {
+				var curItem = false;
+	
+				$(this).on('click', function(e) {
+					var item = $(this);
+					if (item[0] != curItem[0]) {
+						e.preventDefault();
+						curItem = item;
+					}
+				});
+	
+				$(document).on('click touchstart MSPointerDown', handleTouch); 
+				
+				function handleTouch(e) {
+					var resetItem = true,
+						parents = $(e.target).parents();
+	
+					for (var i = 0; i < parents.length; i++)
+						if (parents[i] == curItem[0])
+							resetItem = false;
+	
+					if(resetItem)
+						curItem = false;
+				};
 			});
-		});
-		return this;
+		}
+		return this;	
 	};
 })(jQuery, window, document);
