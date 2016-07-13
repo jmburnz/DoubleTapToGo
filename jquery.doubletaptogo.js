bbac4b6d53452e7918a696df307dc4d71d696faa
jmburnz <jeff@adaptivethemes.com>
2016-07-13 19:57:35 +1200

    Initial commit

--- jquery.doubletaptogo.js
+++ jquery.doubletaptogo.js
@@ -1,51 +1,59 @@
-/* 
-Double Tap to Go
-Author: Graffino (http://www.graffino.com)
-Version: 0.3
-Originally by Osvaldas Valutis, www.osvaldas.info	
-Available for use under the MIT License
-*/
+/**
+ * Double Tap to Go
+ *
+ * Fork: by Jeff Burnz https://github.com/jmburnz/DoubleTapToGo
+ * - Remove win7 mobile check.
+ * - Rewrite to Drupal coding standards.
+ *
+ * Originally by Osvaldas Valutis, www.osvaldas.info
+ * unbind & other improvements by https://github.com/zenopopovici/DoubleTapToGo
+ * License MIT & GPL 2.0
+ *
+ * TODO: request for cdnjs.com: https://github.com/cdnjs/cdnjs/issues/8439
+ */
+;(function($, window, document) {
 
-;(function($, window, document, undefined) {
+	"use strict";
+
 	$.fn.doubleTapToGo = function(action) {
-
-		if (!('ontouchstart' in window) &&
-			!navigator.msMaxTouchPoints &&
-			!navigator.userAgent.toLowerCase().match( /windows phone os 7/i )) return false;
 
 		if (action === 'unbind') {
 			this.each(function() {
 				$(this).off();
-				$(document).off('click touchstart MSPointerDown', handleTouch);	
+				$(document).off('click touchstart MSPointerDown', handleTouch);
 			});
-
-		} else {
+		}
+		else {
 			this.each(function() {
-				var curItem = false;
-	
+				var current_item = false;
+
 				$(this).on('click', function(e) {
 					var item = $(this);
-					if (item[0] != curItem[0]) {
+
+					if (item[0] != current_item[0]) {
 						e.preventDefault();
-						curItem = item;
+						current_item = item;
 					}
 				});
-	
-				$(document).on('click touchstart MSPointerDown', handleTouch); 
-				
+
+				$(document).on('click touchstart MSPointerDown', handleTouch);
+
 				function handleTouch(e) {
-					var resetItem = true,
-						parents = $(e.target).parents();
-	
-					for (var i = 0; i < parents.length; i++)
-						if (parents[i] == curItem[0])
-							resetItem = false;
-	
-					if(resetItem)
-						curItem = false;
+					var reset_item = true,
+						  parents = $(e.target).parents();
+
+					for (var i = 0; i < parents.length; i++) {
+						if (parents[i] == current_item[0]) {
+							reset_item = false;
+						}
+						if (reset_item) {
+							current_item = false;
+						}
+					}
 				}
 			});
 		}
-		return this;	
+
+		return this;
 	};
 })(jQuery, window, document);
