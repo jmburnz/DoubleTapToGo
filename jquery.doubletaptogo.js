45a3179be7f490d0fde3e509c033b9c0f88e6aa4
jmburnz <jeff@adaptivethemes.com>
2016-07-13 20:35:08 +1200

    Ops, control structure mistake :)

--- jquery.doubletaptogo.js
+++ jquery.doubletaptogo.js
@@ -8,8 +8,6 @@
  * Originally by Osvaldas Valutis, www.osvaldas.info
  * unbind & other improvements by https://github.com/zenopopovici/DoubleTapToGo
  * License MIT & GPL 2.0
- *
- * TODO: request for cdnjs.com: https://github.com/cdnjs/cdnjs/issues/8439
  */
 ;(function($, window, document) {
 
@@ -25,31 +23,32 @@
 		}
 		else {
 			this.each(function() {
-				var current_item = false;
+				var currentItem = false;
 
 				$(this).on('click', function(e) {
 					var item = $(this);
 
-					if (item[0] != current_item[0]) {
+					if (item[0] != currentItem[0]) {
 						e.preventDefault();
-						current_item = item;
+            currentItem = item;
 					}
 				});
 
 				$(document).on('click touchstart MSPointerDown', handleTouch);
 
 				function handleTouch(e) {
-					var reset_item = true,
-						  parents = $(e.target).parents();
+          var resetItem = true,
+            parents = $(e.target).parents();
 
-					for (var i = 0; i < parents.length; i++) {
-						if (parents[i] == current_item[0]) {
-							reset_item = false;
-						}
-						if (reset_item) {
-							current_item = false;
+          for (var i = 0; i < parents.length; i++) {
+						if (parents[i] == currentItem[0]) {
+							resetItem = false;
 						}
 					}
+
+          if (resetItem) {
+            currentItem = false;
+          }
 				}
 			});
 		}
